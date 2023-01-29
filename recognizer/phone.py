1a9a73534c8a5b1e48bbe3ffcd698c547338d557
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2023-01-28 21:49:34 -0800

    being able to get all the phones

--- recognizer/phone.py
+++ recognizer/phone.py
@@ -7,9 +7,12 @@ from scipy.io import wavfile
 class Phone():
 
     def __init__(self, data, transcription) -> None:
-        self.data = None
+        self.data = data
         self.mfcc_seq = None
-        self.transcription = None
+        self.transcription = transcription
+
+    def __str__(self) -> str:
+        return f"{self.transcription}"
 
     def get_mfcc_seq(self):
         pass
@@ -26,10 +29,20 @@ class File():
     def __str__(self) -> str:
         return f"path = {self.path}, name = {self.name}, wav = {self.wav}, phn = {self.phn}"
 
+    def get_phones(self):
+        phones = []
+        for line in self.phn:
+            start, end, transcription = line.split()
+            start, end = int(start), int(end)
+            wav_data = self.wav[start:end]
+            phone = Phone(wav_data, transcription)
+            phones.append(phone)
+        return phones
+
 
 # root should be given as the absolute path
 # return files
-def get_all_matched_files(root: str):
+def get_all_matched_files(root: str) -> list[File]:
     phn_re = re.compile(r".+\.PHN")
     matched_files = []
     for dirpath, _, filenames in os.walk(root):
@@ -54,10 +67,13 @@ def read_files(files):
     return files
 
 
-# get the paths of all .wav and .PHN files in the training set
-train_set_path = "/Users/zhuyifang/Downloads/archive/data/TRAIN"
-wav_re = re.compile(r".+WAV\.wav")
-files = get_all_matched_files(train_set_path)
-read_files(files)
-for line in files[0].phn:
-    print(line.split())
+if __name__ == "__main__":
+    # get the paths of all .wav and .PHN files in the training set
+    train_set_path = "/Users/zhuyifang/Downloads/archive/data/TRAIN"
+    wav_re = re.compile(r".+WAV\.wav")
+    files = get_all_matched_files(train_set_path)
+    read_files(files)
+    phones = []
+    for file in files:
+        phones += file.get_phones()
+    print(phones[0].data)
