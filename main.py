619aab5196e34a30329504cf22c35f8c42e3a6b4
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2023-02-11 21:28:57 -0800

    build confusion matrix done

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ import random
 from collections import Counter
 import heapq
 from scipy.io import wavfile
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 
 TIMIT = Path("/Users/zhuyifang/Downloads/archive")
 #TIMIT = Path("/home/bart/work/reed-theses/zhu-thesis/timit")
@@ -107,83 +108,91 @@ def drop_ignored_phones(phones: list[Phone]) -> list[Phone]:
                phones))
 
 
-def group_phones(phones: list[Phone]) -> dict[str, set[Phone]]:
+def group_phones(phones: list[Phone]) -> dict[str, list[Phone]]:
     res = {
-        'ix': {},
-        'iy': {},
-        's': {},
-        'r': {},
-        'n/en/nx': {},
-        'l': {},
-        'tcl': {},
-        'kcl': {},
-        'ih': {},
-        'dcl': {},
-        'k': {},
-        't': {},
-        'm/em': {},
-        'eh': {},
-        'ae': {},
-        'axr/er': {},
-        'ax': {},
-        'z': {},
-        'd': {},
-        'q': {},
-        'w': {},
-        'ao': {},
-        'aa': {},
-        'dh': {},
-        'pcl': {},
-        'p': {},
-        'dx': {},
-        'f': {},
-        'b': {},
-        'ah': {},
-        'ay': {},
-        'gcl': {},
-        'ey': {},
-        'sh': {},
-        'ow': {},
-        'bcl': {},
-        'g': {},
-        'v': {},
-        'y': {},
-        'ux': {},
-        'ng/eng': {},
-        'jh': {},
-        'hv': {},
-        'hh': {},
-        'el': {},
-        'th': {},
-        'oy': {},
-        'ch': {},
-        'uh': {},
-        'aw': {},
-        'uw': {},
-        'ax-h': {},
-        'zh': {}
+        'ix': [],
+        'iy': [],
+        's': [],
+        'r': [],
+        'n/en/nx': [],
+        'l': [],
+        'tcl': [],
+        'kcl': [],
+        'ih': [],
+        'dcl': [],
+        'k': [],
+        't': [],
+        'm/em': [],
+        'eh': [],
+        'ae': [],
+        'axr/er': [],
+        'ax': [],
+        'z': [],
+        'd': [],
+        'q': [],
+        'w': [],
+        'ao': [],
+        'aa': [],
+        'dh': [],
+        'pcl': [],
+        'p': [],
+        'dx': [],
+        'f': [],
+        'b': [],
+        'ah': [],
+        'ay': [],
+        'gcl': [],
+        'ey': [],
+        'sh': [],
+        'ow': [],
+        'bcl': [],
+        'g': [],
+        'v': [],
+        'y': [],
+        'ux': [],
+        'ng/eng': [],
+        'jh': [],
+        'hv': [],
+        'hh': [],
+        'el': [],
+        'th': [],
+        'oy': [],
+        'ch': [],
+        'uh': [],
+        'aw': [],
+        'uw': [],
+        'ax-h': [],
+        'zh': []
     }
 
     for phone in phones:
         # fold the 4 groups
         if phone.transcription in GROUP_1:
             phone.transcription = 'axr/er'
-            res['axr/er'].add(phone)
+            res['axr/er'].append(phone)
         elif phone.transcription in GROUP_2:
             phone.transcription = 'm/em'
-            res['m/em'].add(phone)
+            res['m/em'].append(phone)
         elif phone.transcription in GROUP_3:
             phone.transcription = 'n/en/nx'
-            res['n/en/nx'].add(phone)
+            res['n/en/nx'].append(phone)
         elif phone.transcription in GROUP_4:
             phone.transcription = 'ng/eng'
-            res['ng/eng'].add(phone)
+            res['ng/eng'].append(phone)
         else:
-            res[phone.transcription].add(phone)
+            res[phone.transcription].append(phone)
     return res
 
 
-def predict_phone(train_set_phones, test_phone):
+def get_n_from_each_group(phone_groups: dict[str, list[Phone]],
+                          n: int) -> list[Phone]:
+    res = []
+    for group in phone_groups.values():
+        res += random.sample(group, n)
+    return res
+
+
+def predict_phone(train_set_phones: list[Phone], test_phone: Phone) -> str:
 
     # using KNN to find the nearest neighbor
     k = 10
@@ -211,41 +220,27 @@ def predict_phone(train_set_phones, test_phone):
     return predicted_phone
 
 
-def test_accuracy(train_set_phones: list[Phone], test_set_phones: list[Phone]):
-    # test accuracy
+def test(train_set_phones: list[Phone], test_phones: list[Phone]):
+    true_lst = []
+    pred_lst = []
     correct_num = 0
-
-    # iterate all the phones in the test set
-    nphones = 100
-    test_phones = random.sample(test_set_phones, nphones)
-
-    predicted_phone = predict_phone(train_set_phones, test_phones)
     for test_phone in test_phones:
-        # if the prediction is correct
+        predicted_phone = predict_phone(train_set_phones, test_phone)
         if predicted_phone == test_phone.transcription:
             correct_num += 1
-            print("correct")
-
-        # if the prediction and test phone are in the same group
-        else:
-            for i in range(len(GROUPS)):
-                if predicted_phone in GROUPS[
-                        i] and test_phone.transcription in GROUPS[i]:
-                    correct_num += 1
-                    print("correct")
-                    break
-        print(
-            f'predicted phone is: {predicted_phone}, actual phone is: {test_phone.transcription}'
-        )
-    # print the accuracy
-    print(f"Accuracy: {correct_num / nphones}")
+        true_lst.append(test_phone.transcription)
+        pred_lst.append(predicted_phone)
+    print(f"The accuracy is {correct_num / len(test_phones)}")
+    return true_lst, pred_lst
 
 
 if __name__ == "__main__":
     train_set_phones, test_set_phones = get_phones()
     train_set_phones = drop_ignored_phones(train_set_phones)
+    train_phones = group_phones(train_set_phones)
     test_set_phones = drop_ignored_phones(test_set_phones)
     test_phones = group_phones(test_set_phones)
+    test_phones = get_n_from_each_group(test_phones, 1)
 
     # # get the number of each phone in the training set and test set
     # train_set_counter = Counter()
@@ -256,10 +251,7 @@ if __name__ == "__main__":
     # for phone in test_set_phones:
     #     test_set_counter[phone.transcription] += 1
     # print(f"The stats of the testing set: {test_set_counter}")
-
-    # test accuracy
-    test_accuracy(train_set_phones, test_set_phones)
-
+    true_lst, pred_lst = test(train_set_phones, test_phones)
     # confusion matrix test
     labels = [
         'ix', 'iy', 's', 'r', 'n/en/nx', 'l', 'tcl', 'kcl', 'ih', 'dcl', 'k',
@@ -268,3 +260,7 @@ if __name__ == "__main__":
         'ow', 'bcl', 'g', 'v', 'y', 'ux', 'ng/eng', 'jh', 'hv', 'hh', 'el',
         'th', 'oy', 'ch', 'uh', 'aw', 'uw', 'ax-h', 'zh'
     ]
+    cm_display = ConfusionMatrixDisplay(confusion_matrix(true_lst, pred_lst),
+                                        display_labels=labels)
+    cm_display.plot()
+    plt.show()
