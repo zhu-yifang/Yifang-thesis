337d6ef7f00837585b5cda347f988589a0211aa9
Yifang Zhu <70597936+zhu-yifang@users.noreply.github.com>
2023-07-26 00:27:18 -0700

    Did a code review with ChatGPT

--- utils/util.py
+++ utils/util.py
@@ -6,6 +6,7 @@ import librosa
 from python_speech_features import mfcc
 import sounddevice as sd
 import tensorflow as tf
+from typing import Union
 
 # Defines the global variables here
 # Path to the dataset
@@ -112,68 +113,69 @@ def read_metadata(metadata_path: Path) -> pd.DataFrame:
     return pd.read_csv(metadata_path)
 
 
-def get_core_test_set(pd: pd.DataFrame) -> pd.DataFrame:
+def get_core_test_set(df: pd.DataFrame) -> pd.DataFrame:
     """
     Get the core test set from the test set.
 
-    :param pd: a DataFrame from `read_metadata`
-    :type pd: pd.DataFrame
+    :param df: a DataFrame from `read_metadata`
+    :type df: pd.DataFrame
     :return: a DataFrame
     :rtype: pd.DataFrame
     """
-    return pd[pd["speaker_id"].isin(CORE_TEST_SET_SPEAKER_IDS)]
+    return df[df["speaker_id"].isin(CORE_TEST_SET_SPEAKER_IDS)]
 
 
-def get_paths_no_ext(pd: pd.DataFrame) -> pd.Series:
+def get_paths_no_ext(df: pd.DataFrame) -> pd.Series:
     """
     Get the paths to all files in the training set or test set without extensions.
 
-    :param pd: a DataFrame, either from `read_metadata` or `get_core_test_set`
-    :type pd: pd.DataFrame
+    :param df: a DataFrame, either from `read_metadata` or `get_core_test_set`
+    :type df: pd.DataFrame
     :return: a Series of file extensions
     :rtype: pd.Series
     """
-    paths_no_ext = pd["path_from_data_dir"].str.split(".").str[0]
+    paths_no_ext = df["path_from_data_dir"].str.split(".").str[0]
     paths_no_ext.drop_duplicates(inplace=True)
     # Reset the index of `train_path_no_ext`
     paths_no_ext.reset_index(drop=True, inplace=True)
     return paths_no_ext
 
 
-def get_samples(paths_no_ext: pd.Series) -> pd.DataFrame:
-    """
-    Get the samples from the path without extension.
-    Return a DataFrame with the following columns:
-    - `class`: a index representing the phoneme
-    - `phonetic_transcription`: the phonetic transcription
-    - `wav_array`: the wav array
+def read_wav_file(path: str) -> np.ndarray:
+    wav_path = TIMIT / "data" / Path(path + ".WAV.wav")
+    wav_array, _ = librosa.load(wav_path, sr=None)
+    return wav_array
 
-    :param paths_no_ext: a Series of paths without extension
-    :type paths_no_ext: pd.Series
-    :return: a DataFrame
-    :rtype: pd.DataFrame
-    """
-    wav_arries = []
-    phonetic_transcriptions = []
-    classes = []
+
+def read_phn_file(path: str) -> list[tuple[str, str, str]]:
+    phn_path = TIMIT / "data" / Path(path + ".phn")
+    with open(phn_path, "r") as f:
+        return [line.split() for line in f]
+
+
+def get_samples_from_file(
+        path: str) -> list[dict[str, Union[int, str, np.ndarray]]]:
+    wav_array = read_wav_file(path)
+    phn_data = read_phn_file(path)
+
+    samples = []
+    for start, end, transcription in phn_data:
+        start, end = int(start), int(end)
+        sample = {
+            "class": PHONEME_TO_INDEX[transcription],
+            "phonetic_transcription": transcription,
+            "wav_array": wav_array[start:end]
+        }
+        samples.append(sample)
+    return samples
+
+
+def get_samples(paths_no_ext: pd.Series) -> pd.DataFrame:
+    all_samples = []
     for path in paths_no_ext:
-        wav_path = TIMIT / "data" / Path(path + ".WAV.wav")
-        wav_array, samplerate = librosa.load(
-            wav_path,
-            sr=None)  # set `sr=None` to get the original sampling rate
-        phn_path = TIMIT / "data" / Path(path + ".phn")
-        with open(phn_path, "r") as f:
-            for line in f:
-                start, end, transcription = line.split()
-                start, end = int(start), int(end)
-                wav_arries.append(wav_array[start:end])
-                phonetic_transcriptions.append(transcription)
-                classes.append(PHONEME_TO_INDEX[transcription])
-    return pd.DataFrame({
-        "class": classes,
-        "phonetic_transcription": phonetic_transcriptions,
-        "wav_array": wav_arries
-    })
+        samples_from_file = get_samples_from_file(path)
+        all_samples.extend(samples_from_file)
+    return pd.DataFrame(all_samples)
 
 
 # To make all the features with the same length, we need to use `librosa` to stretch or shrink the `wav_array` to the same length. Because the mean length of `wav_array` is 1279.5, we will stretch the `wav_array` to the length of 1024. And add a new column `wav_array_stretched` to `train_data_new` to store the stretched `wav_array`
@@ -203,46 +205,42 @@ def get_mfcc_vect(wav_array: np.ndarray) -> np.ndarray:
     :rtype: np.ndarray
     """
     assert len(wav_array) == 1024, "The length of the wav array must be 1024"
+    mfcc_order0 = mfcc(wav_array, samplerate=16000).reshape(-1)
     mfcc_vect = np.concatenate(
         mfcc(wav_array, samplerate=16000).reshape(-1),
-        librosa.feature.delta(mfcc(wav_array, samplerate=16000)).reshape(-1),
-        librosa.feature.delta(mfcc(wav_array, samplerate=16000),
-                              order=2).reshape(-1),
+        librosa.feature.delta(mfcc_order0).reshape(-1),
+        librosa.feature.delta(mfcc_order0, order=2).reshape(-1),
     )
     assert len(mfcc_vect) == 195, "The length of the MFCC vector must be 195"
     return mfcc_vect
 
 
 # Calculate the MFCC vectors for all the samples in the training set and test set
-def add_mfcc_vects(pd: pd.DataFrame) -> pd.DataFrame:
+def add_mfcc_vects(df: pd.DataFrame) -> pd.DataFrame:
     """
     Add the MFCC vectors to the DataFrame.
 
-    :param pd: a DataFrame, either from `read_metadata` or `get_core_test_set`
-    :type pd: pd.DataFrame
+    :param df: a DataFrame, either from `read_metadata` or `get_core_test_set`
+    :type df: pd.DataFrame
     :return: a DataFrame
     :rtype: pd.DataFrame
     """
-    mfcc_vects = []
-    for wav_array in pd["wav_array"]:
-        wav_array_stretched = stretch_wav_array(wav_array)
-        mfcc_vect = get_mfcc_vect(wav_array_stretched)
-        mfcc_vects.append(mfcc_vect)
-    pd["mfcc_vect"] = mfcc_vects
-    return pd
+    df["mfcc_vect"] = df["wav_array"].apply(stretch_wav_array).apply(
+        get_mfcc_vect)
+    return df
 
 
-def get_X_y(pd: pd.DataFrame) -> (np.ndarray, np.ndarray):
+def get_X_y(df: pd.DataFrame) -> (np.ndarray, np.ndarray):
     """
     Get the X and y from the DataFrame.
 
-    :param pd: a `DataFrame` contains the `mfcc_vect` column
-    :type pd: pd.DataFrame
+    :param df: a `DataFrame` contains the `mfcc_vect` column
+    :type df: pd.DataFrame
     :return: a tuple of X and y
     :rtype: (np.ndarray, np.ndarray)
     """
-    X = np.array(pd["mfcc_vect"].tolist())
-    y = np.array(pd["class"].tolist())
+    X = np.array(df["mfcc_vect"].tolist())
+    y = np.array(df["class"].tolist())
     return X, y
 
 
